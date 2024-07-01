24e8b275055974e481ea515dfdca83de81343724
wangjunfei <471612627@qq.com>
2024-07-02 00:46:09 +0800

    example data

---
+++ src/utils/data.ts
@@ -0,0 +1,9 @@
+// /src/data/data.js
+export const dataList = [
+    { id: 'Ch3ef', title: 'Data 1', content: 'This is the first data content.' },
+    { id: 'Chef', title: 'Data 2', content: 'This is the second data content.' },
+    { id: 'Gemini', title: 'Data 3', content: 'This is the third data content.' },
+    { id: 'PsySafe', title: 'Data 4', content: 'This is the fourth data content.' },
+    { id: 'SALAD-Bench', title: 'Data 5', content: 'This is the fifth data content.' },
+  ];
+  
